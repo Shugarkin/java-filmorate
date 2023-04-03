ec61696d93a946d3942ed2d8146d5412a58b7bc3
shugarkin <alexandr.shugarkin@yandex.ru>
2023-04-03 06:32:45 +0300

    feat: добавлено логирование в класс HandlerException

--- src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
@@ -16,18 +16,21 @@ public class HandlerException {
     @ExceptionHandler({FilmIsNotFoundException.class, UserIsNotFoundException.class, IncorrectIDException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleUserIncorrect(final RuntimeException e) {
+        log.warn("Ошибка пользователя.");
         return new ErrorResponse("Ошибка пользователя", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse serverError(final Throwable e) {
+        log.warn("Неизвестная ошибка.");
         return new ErrorResponse("Что-то пошло не так(", e.getMessage());
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse badRequest(final Exception e) {
+        log.warn("Ошибка валидации.");
         return new ErrorResponse("Ошибка валидации", e.getMessage());
     }
 }
