d96e6c74f55e0e9c69902ae6c16765a8906ed67d
shugarkin <alexandr.shugarkin@yandex.ru>
2023-05-01 14:22:00 +0300

    style: убрал неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
-import java.util.Optional;
 
 public interface UserStorage {
 
