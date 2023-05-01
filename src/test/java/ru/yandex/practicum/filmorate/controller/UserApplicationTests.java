07817c05a0cf3b539fbe2d7c8cf2e3557e83022e
shugarkin <alexandr.shugarkin@yandex.ru>
2023-05-01 14:22:31 +0300

    style: убрал неиспользуемый импорт

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserApplicationTests.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
