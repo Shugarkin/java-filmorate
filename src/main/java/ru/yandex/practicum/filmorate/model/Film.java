9643f8c261280f1c7ae527be45d9e9a72af2d2b5
Andrey <andrei4-09@mail.ru>
2023-05-22 10:25:13 +0300

    fix: добавлены в коммит ранее удаленные файлы - без них тест на ГХ не проходит

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.validator.ReleaseDate;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+@Data
+@Builder
+public class Film {
+
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @Size(max = 200)
+    @NotNull
+    private String description;
+
+    @NotNull
+    @ReleaseDate
+    private LocalDate releaseDate;
+
+    @Positive
+    private long duration;
+
+    private LinkedHashSet<Genre> genres;
+
+    @NotNull
+    private Mpa mpa;
+
+    public void addGenre(Genre genre) {
+       genres.add(genre);
+    }
+}
