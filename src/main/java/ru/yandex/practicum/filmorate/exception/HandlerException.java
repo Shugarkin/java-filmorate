e67f0b0fe307559ccb82674d92152468be699498
Andrey <andrei4-09@mail.ru>
2023-05-22 21:18:53 +0300

    fix: удалены неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
-
 @RestControllerAdvice("ru.yandex.practicum.filmorate.controller")
 @Slf4j
 public class HandlerException {
