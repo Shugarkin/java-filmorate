8606111130659530b69859189d2a2945b693d77a
Andrey <andrei4-09@mail.ru>
2023-05-22 20:59:29 +0300

    fix: удален неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
-import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
