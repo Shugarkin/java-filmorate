db9f607124eb40b064bd52334be0ca89fd948ea8
Andrey <andrei4-09@mail.ru>
2023-05-22 21:16:15 +0300

    fix: удалены Exception-ы

--- src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/HandlerException.java
@@ -27,7 +27,7 @@ public class HandlerException {
         return new ErrorResponse("Что-то пошло не так", e.getMessage());
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class, NullPointerException.class})
+    @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse badRequest(final Exception e) {
         log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
