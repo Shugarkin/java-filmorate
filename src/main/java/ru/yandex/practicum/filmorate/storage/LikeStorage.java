dbdc298f5718b65d068dccaae786f6624db2867c
shugarkin <alexandr.shugarkin@yandex.ru>
2023-05-23 11:22:42 +0300

    style: удаленны не нужные импорты #3

--- src/main/java/ru/yandex/practicum/filmorate/storage/LikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/LikeStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface LikeStorage {
