6038f8cfb998c5f4882d727da387ab6de0908777
Shugarkin <alexandr.shugarkin@yandex.ru>
2023-05-29 17:26:55 +0300

    style: удалил импорт

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.storage.RecommendationsStorage;
 
