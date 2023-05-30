21602a96e2cae7aee6e02bb6ec42793c491ed8c1
Freddycs14 <freddycs14@gmail.com>
2023-05-30 17:09:10 +0400

    fix: checkStyle 2

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
-import java.util.Set;
 
 public interface FilmStorage {
 
